e61079ae322fff3e241a29d97efb409bc9cb8ee0
Doron Sinai <sinai.doron+github@gmail.com>
2018-06-20 16:01:49 +0300

    update example

--- examples/src/app.js
+++ examples/src/app.js
@@ -2,29 +2,31 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import { Provider, connect } from 'react-redux';
 import { default as Store } from './Redux/Store';
-import { Notifications, NotificationsActions } from 'amaryllis-notifications';
+import { Notifications, NotificationsActions, AMARYLLIS_TYPES} from 'amaryllis-notifications';
 
-function mapActionsToProps(dispatch){
-    return {
-        notify: (type, title, message, options) => {
-            return dispatch(NotificationsActions.showStaticNotification (type, title, message, options));
-        },
-        notifyTimeout: (type, title, message, options) => {
-            return dispatch(NotificationsActions.showTimedNotification(type, title, message, options))
-        }
-    }
+const mapActionsToProps = {
+    notify: NotificationsActions.notify
 }
 
 class Page extends React.Component {
     componentDidMount(){
-        this.props.notify('warning', "Warning!", "Pay attention to dove", {faicon:"fa-dove"});
-        this.props.notify('info', "Info!", "There is a frog on the left side", {faicon:"fa-frog"});
-        this.props.notify('success', "Success!", "You found a kiwi", {
+        this.props.notify("Warning!", "Pay attention to dove", {faicon:"fa-dove", type:AMARYLLIS_TYPES.INFO});
+        this.props.notify("Info!", "There is a frog on the left side", {faicon:"fa-frog", type:AMARYLLIS_TYPES.WARNING});
+        this.props.notify("Success!", "You found a kiwi", {
+            faicon:"fa-kiwi-bird",
+            location: "top-right",
+            type:AMARYLLIS_TYPES.ERROR
+        });
+        this.props.notify("Success!", "You found a kiwi", {
             faicon:"fa-kiwi-bird",
             location: "top-right"
         });
-        this.props.notify('error', "Error!", "Crow Crow Crow", {faicon:"fa-crow"});
-        this.props.notify('happy', "Happy", "This is my happy place", {faicon:"fa-cloud"});
+        this.props.notify("Error!", "Crow Crow Crow", {
+            faicon:"fa-crow",
+            timeout: 0,
+            type:AMARYLLIS_TYPES.SUCCESS
+        });
+        this.props.notify("Happy", "This is my happy place", {faicon:"fa-cloud", type:AMARYLLIS_TYPES.HAPPY});
     }
 
     render(){
