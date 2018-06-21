5fdaa361c382dfea710e5a7061e9098d2ece5be7
Doron Sinai <sinai.doron+github@gmail.com>
2018-06-21 12:11:55 +0300

    Update sample app

--- examples/src/app.js
+++ examples/src/app.js
@@ -10,23 +10,23 @@ const mapActionsToProps = {
 
 class Page extends React.Component {
     componentDidMount(){
-        setDefaults({timeout:0, styles:{container:{color:'green'}}})
-        // this.props.notify("Warning!", "Pay attention to dove", {faicon:"fa-dove", type:AMARYLLIS_TYPES.INFO});
-        // this.props.notify("Info!", "There is a frog on the left side, with a long text", {faicon:"fa-frog", type:AMARYLLIS_TYPES.WARNING});
-        // this.props.notify("Success!", "You found a kiwi", {
-        //     faicon:"fa-kiwi-bird",
-        //     location: "top-right",
-        //     type:AMARYLLIS_TYPES.ERROR
-        // });
-        // this.props.notify("Success!", "You found a kiwi", {
-        //     location: "top-right"
-        // });
-        // this.props.notify("Error!", "Crow Crow Crow", {
-        //     faicon:"fa-crow",
-        //     timeout: 0,
-        //     type:AMARYLLIS_TYPES.SUCCESS
-        // });
-        // this.props.notify("Happy", "This is my happy place");
+        setDefaults({timeout:0, styles:{}})
+        this.props.notify("Warning!", "Pay attention to dove", {icon:"fa-dove", type:AMARYLLIS_TYPES.INFO});
+        this.props.notify("Info!", "There is a frog on the left side, with a long text", {icon:"fa-frog", type:AMARYLLIS_TYPES.WARNING});
+        this.props.notify("Success!", "You found a kiwi", {
+            icon:"fa-kiwi-bird",
+            location: "top-right",
+            type:AMARYLLIS_TYPES.ERROR
+        });
+        this.props.notify("Success!", "You found a kiwi", {
+            location: "top-right"
+        });
+        this.props.notify("Error!", "Crow Crow Crow", {
+            icon:"fa-crow",
+            timeout: 0,
+            type:AMARYLLIS_TYPES.SUCCESS
+        });
+        this.props.notify("Happy", "This is my happy place");
         this.props.notify("Happy Happy", "I have a pragrapgh ", {action: {text:"say hello",func:()=>this.props.notify("Hello", "World", {icon:'fa-star', type: AMARYLLIS_TYPES.INFO})}})
         this.props.notify("Happy Happy", "I have a pragrapgh with long text and some more data i wnat to show", {icon:'fa-kiwi-bird', styles:{icon:{color:'pink'},container:{color:'red'},message:{color:'#d3d3d3'}}})
     }
@@ -42,7 +42,7 @@ Page = connect(null, mapActionsToProps)(Page)
 ReactDOM.render(
     <Provider store={Store}>
         <React.Fragment>
-            <Notifications />
+            <Notifications animation={"slide"}/>
             <Page />
         </React.Fragment>
     </Provider>,
