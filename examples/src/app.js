b3b286354401529fadaadb0fb365fdd8802ae9a4
Doron Sinai <sinai.doron+github@gmail.com>
2018-06-21 21:11:44 +0300

    Update the app example

--- examples/src/app.js
+++ examples/src/app.js
@@ -11,25 +11,44 @@ const mapActionsToProps = {
 
 class Page extends React.Component {
     componentDidMount(){
-        setDefaults({timeout:5000, styles:{}})
-        this.props.notify("Warning!", "Pay attention to dove", {icon:"fa-dove", type:AMARYLLIS_TYPES.INFO});
-        this.props.notify("Info!", "There is a frog on the left side, with a long text", {icon:"fa-frog", type:AMARYLLIS_TYPES.WARNING});
-        this.props.notify("Success!", "You found a kiwi", {
+        const { ...props } = this.props;
+        setDefaults({timeout:0, styles:{}})
+        props.notify("Leonard Cohen", "Remember when I moved in you and the holy dove was moving too, and every breath we drew was, Hallelujah.",
+             {icon:"fa-dove", type:AMARYLLIS_TYPES.WARNING});
+        props.notify(" E.L. James:", "You have to kiss a lot of frogs before you find your prince", {icon:"fa-frog", type:AMARYLLIS_TYPES.INFO});
+        props.notify("William Shakespeare", "I had rather hear my dog bark at a crow, than a man swear he loves me", 
+            {icon:"fa-crow", type:AMARYLLIS_TYPES.ERROR, location: 'top-right'});
+        props.notify("Success!", "You found a kiwi", {
             icon:"fa-kiwi-bird",
             location: "top-right",
-            type:AMARYLLIS_TYPES.ERROR
-        });
-        this.props.notify("Success!", "You found a kiwi", {
-            location: "top-right"
-        });
-        this.props.notify("Error!", "Crow Crow Crow", {
-            icon:"fa-crow",
-            timeout: 0,
             type:AMARYLLIS_TYPES.SUCCESS
         });
-        this.props.notify("Happy", "This is my happy place");
-        this.props.notify("Happy Happy", "I have a pragrapgh ", {action: {text:"say hello",func:()=>this.props.notify("Hello", "World", {icon:'fa-star', type: AMARYLLIS_TYPES.INFO})}})
-        this.props.notify("Happy Happy", "I have a pragrapgh with long text and some more data i wnat to show", {icon:'fa-kiwi-bird', styles:{icon:{color:'pink'},container:{color:'red'},message:{color:'#d3d3d3'}}})
+        props.notify("Happy!", "Happy Happy, Joy Joy", {
+            location: "top-right",
+            type:AMARYLLIS_TYPES.HAPPY
+        });
+
+        props.notify("Custom", "Configurable styles", {
+            icon:"fa-feather",
+            timeout: 0,
+            type:AMARYLLIS_TYPES.SUCCESS,
+            styles:{
+                container:{
+                    backgroundColor:'#54577c'
+                },
+                title:{
+                    color: '#eac435'
+                },
+                message:{
+                    color: '#03cea4'
+                },
+                icon:{
+                    color: '#3e92cc'
+                }
+            }
+        });
+        props.notify("Action", "Attach a callback and an action ", 
+            {action: {text:"say hello",func:()=>this.props.notify("Hello", "World", {icon:'fa-star', type: AMARYLLIS_TYPES.INFO})}})
     }
 
     render(){
